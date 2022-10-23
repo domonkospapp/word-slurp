d48334f033f39533b4f8f43e4c85d486b59f11f3
Domonkos Papp <domonkos.papp@regnology.net>
2022-10-23 17:58:14 +0200

    Initial commit

---
+++ src/main/groovy/com/dpapp/wordlearning/WordLearningApplication.groovy
@@ -0,0 +1,13 @@
+package com.dpapp.wordlearning
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class WordLearningApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(WordLearningApplication, args)
+    }
+
+}
---
+++ src/test/groovy/com/dpapp/wordlearning/WordLearningApplicationTests.groovy
@@ -0,0 +1,13 @@
+package com.dpapp.wordlearning
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WordLearningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
