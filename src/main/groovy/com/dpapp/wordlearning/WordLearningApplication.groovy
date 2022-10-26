977289632a73fcdb91658c5a0c5b90a157af37b3
Domonkos Papp <domonkos.papp@regnology.net>
2022-10-26 21:37:15 +0200

    Enable cors

--- src/main/groovy/com/dpapp/wordlearning/WordLearningApplication.groovy
+++ src/main/groovy/com/dpapp/wordlearning/WordLearningApplication.groovy
@@ -2,6 +2,9 @@ package com.dpapp.wordlearning
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @SpringBootApplication
 class WordLearningApplication {
@@ -9,5 +12,16 @@ class WordLearningApplication {
     static void main(String[] args) {
         SpringApplication.run(WordLearningApplication, args)
     }
+
+    @Bean
+    WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*")
+            }
+        }
+    }
+
 
 }
