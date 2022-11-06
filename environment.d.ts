eb69ff1306d040e0a6c7632d4d256babb07fc2cb
Domonkos Papp <domonkos.papp@regnology.net>
2022-11-06 16:27:50 +0100

    Format

--- environment.d.ts
+++ environment.d.ts
@@ -1,10 +1,9 @@
 declare global {
-    namespace NodeJS {
-      interface ProcessEnv {
-        GOOGLE_CLIENT_ID: string;
-        GOOGLE_CLIENT_SECRET: string;
-      }
+  namespace NodeJS {
+    interface ProcessEnv {
+      GOOGLE_CLIENT_ID: string
+      GOOGLE_CLIENT_SECRET: string
     }
+  }
 }
 export {}
-  
