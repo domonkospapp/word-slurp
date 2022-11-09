4b3d821236023ee79faf23d38bed58d3183da955
Domonkos Papp <domonkos.papp@regnology.net>
2022-11-09 20:55:58 +0100

    Create word upload page

---
+++ pages/words/upload.tsx
@@ -0,0 +1,60 @@
+import * as React from 'react'
+import { GetServerSideProps } from 'next'
+import { getToken } from 'next-auth/jwt'
+import { FormData, Blob } from 'formdata-node'
+import { IncomingMessage } from 'http'
+
+const FILE_UPLOAD_BASE_URL = `${process.env.BACKEND_BASE_URL}/words/translations`
+
+const createBody = (file: string) => {
+  const form = new FormData()
+  const blob = new Blob([file], { type: 'text/csv' })
+  form.set('translations', blob, 'translation.csv')
+  return form
+}
+
+const createAuthHeader = async (req: IncomingMessage) => {
+  const token = await getToken({ req })
+  return {
+    Authorization: `Bearer ${token?.id_token}`,
+  }
+}
+
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+  if (req.method == 'POST') {
+    let body = ''
+    req.on('data', (chunk) => {
+      body += chunk
+    })
+    req.on('end', async () => {
+      await fetch(FILE_UPLOAD_BASE_URL, {
+        method: 'POST',
+        headers: await createAuthHeader(req),
+        body: createBody(body),
+      })
+    })
+    return {
+      redirect: {
+        permanent: false,
+        destination: '/words',
+      },
+      props: {},
+    }
+  }
+
+  return {
+    props: {},
+  }
+}
+
+const ImportWords = () => {
+  return (
+    <div>
+      <form action="/words/upload" method="POST" encType="multipart/form-data">
+        <input type="file" id="myFile" name="filename" />
+        <input type="submit" />
+      </form>
+    </div>
+  )
+}
+export default ImportWords
