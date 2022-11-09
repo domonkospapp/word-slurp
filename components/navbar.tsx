220a7505e5eefd1c1f01c392be3bfb6cc6fc8a8a
Domonkos Papp <domonkos.papp@regnology.net>
2022-11-09 20:54:06 +0100

    Fix menu

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 const Navbar = () => (
   <>
     <Link href="/words">My words</Link> |<Link href="learning">Learning</Link> |
-    <Link href="words/import">Import words</Link> |
+    <Link href="/words/upload">Import words</Link> |
     <button onClick={() => signIn('google')}>Login</button>
     <button onClick={() => signOut()}>Logout</button>
     <hr />
