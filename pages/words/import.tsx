aa1720a04b9c41297a8f5de324af84bb038549df
Domonkos Papp <domonkos.papp@regnology.net>
2022-11-10 21:05:03 +0100

    Implement word importer page

---
+++ pages/words/import.tsx
@@ -0,0 +1,58 @@
+import * as React from 'react'
+import { GetServerSideProps } from 'next'
+import { ChangeEvent, useState } from 'react'
+import { parseBody } from 'next/dist/server/api-utils/node'
+import { initAxiosAuthHeaderInterceptor } from '../../axiosUtils'
+import { importWords } from '../../wordApi'
+import axios from 'axios'
+import { useRouter } from 'next/router'
+
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+  if (req.method == 'POST') {
+    const body = await parseBody(req, '1mb')
+    await initAxiosAuthHeaderInterceptor({ req })
+    importWords(body.csv)
+  }
+  return { props: {} }
+}
+
+const ImportWords = () => {
+  const router = useRouter()
+  const [selectedFile, setSelectedFile] = useState<File>()
+  const [error, setError] = useState<string>()
+
+  const changeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files?.[0]) {
+      const file: File = event.target.files[0]
+      if (file.type != 'text/csv') {
+        setError('Not supported file format, please pload a CSV!')
+      } else {
+        setSelectedFile(file)
+        file.text().then((content) => {
+          console.log('local')
+          console.log(content)
+          axios
+            .post('/words/import', { csv: content })
+            .then(() => router.push('/words'))
+        })
+      }
+    }
+  }
+
+  return (
+    <div>
+      <input type="file" name="file" onChange={changeHandler} />
+      {selectedFile && !error ? (
+        <div>
+          <p>Filename: {selectedFile.name}</p>
+          <p>Filetype: {selectedFile.type}</p>
+          <p>Size in bytes: {selectedFile.size}</p>
+        </div>
+      ) : (
+        <p>Select an exported Google Translate csv file to import words</p>
+      )}
+      {error && <p>{error}</p>}
+    </div>
+  )
+}
+export default ImportWords
