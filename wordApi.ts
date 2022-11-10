30fd13a96f4e369ba7d8ebe8889624f931ce736f
Domonkos Papp <domonkos.papp@regnology.net>
2022-11-10 21:03:01 +0100

    Add import words request

--- wordApi.ts
+++ wordApi.ts
@@ -7,16 +7,16 @@ export const getWords = () => axios.get(BASE_URL_WORDS).then((res) => res.data)
 
 export const createWord = (word: Word) =>
   axios
-    .post(
-      BASE_URL_WORDS,
-      {
-        original: word.original,
-        foreign: word.foreign,
-      },
-      {
-        headers: {
-          'content-type': 'application/json',
-        },
-      }
-    )
+    .post(BASE_URL_WORDS, {
+      original: word.original,
+      foreign: word.foreign,
+    })
     .then((res) => res.data)
+
+export const importWords = (wordCsv: string) => {
+  console.log(JSON.stringify(wordCsv))
+  const body = JSON.stringify({
+    content: wordCsv,
+  })
+  axios.post(`${BASE_URL_WORDS}/import`, body).then((res) => res.data)
+}
