02a6243320a9486510d3e5ccd26dcf9050c6a983
Domonkos Papp <domonkos.papp@regnology.net>
2022-11-10 21:03:44 +0100

    Use correct auth button based on session

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,13 +1,20 @@
-import { signIn, signOut } from 'next-auth/react'
+import { signIn, signOut, useSession } from 'next-auth/react'
 import Link from 'next/link'
 
-const Navbar = () => (
-  <>
-    <Link href="/words">My words</Link> |<Link href="learning">Learning</Link> |
-    <Link href="/words/upload">Import words</Link> |
-    <button onClick={() => signIn('google')}>Login</button>
-    <button onClick={() => signOut()}>Logout</button>
-    <hr />
-  </>
-)
+const Navbar = () => {
+  const { data: session } = useSession()
+  return (
+    <>
+      <Link href="/words">My words</Link> |<Link href="learning">Learning</Link>{' '}
+      |<Link href="/words/import">Import words</Link> |
+      {session?.user ? (
+        <button onClick={() => signOut()}>Logout</button>
+      ) : (
+        <button onClick={() => signIn('google')}>Login</button>
+      )}
+      <hr />
+    </>
+  )
+}
+
 export default Navbar
