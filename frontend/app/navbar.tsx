5eb03cd0f1aaef03a9b81dc842b76e224113ed21
Domonkos Papp <51707177+domonkospapp@users.noreply.github.com>
2022-12-16 00:36:30 +0100

    Fix protected page checks by the middleware
    * Fix signin links
    
    * Do not prefetch pages secured by middleware

--- frontend/app/navbar.tsx
+++ frontend/app/navbar.tsx
@@ -7,9 +7,18 @@ const Navbar = () => {
   const { data: session } = useSession()
   return (
     <>
-      <Link href="/words">My words</Link> |
-      <Link href="/learning">Learning</Link> |
-      <Link href="/words/import">Import words</Link> |
+      <Link href="/words" prefetch={false}>
+        My words
+      </Link>{' '}
+      |
+      <Link href="/learning" prefetch={false}>
+        Learning
+      </Link>{' '}
+      |
+      <Link href="/words/import" prefetch={false}>
+        Import words
+      </Link>{' '}
+      |
       {session?.user ? (
         <button onClick={() => signOut()}>Logout</button>
       ) : (
