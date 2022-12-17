88e776d312dea997029a6393ecb9540f92a22d90
Domonkos Papp <51707177+domonkospapp@users.noreply.github.com>
2022-12-17 12:25:58 +0100

    Update header

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,7 +1,7 @@
 const Home = () => {
   return (
     <>
-      <h1>Word learning application</h1>
+      <h1>Word Slurp</h1>
       <p>Start with importing your words from google translate!</p>
       <a href="https://github.com/domonkospapp/word-learning-frontend/wiki/How-to-import-words-from-Google-Translate">
         How can I import?
