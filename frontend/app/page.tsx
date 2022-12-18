12a4cacf55ea8f4cf53d3023963dbbc52087f6e7
Domonkos Papp <51707177+domonkospapp@users.noreply.github.com>
2022-12-18 20:39:32 +0100

    Add home page description

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,6 +2,7 @@ const Home = () => {
   return (
     <>
       <h1>Word Slurp</h1>
+      <p>The WordSlurp app is a tool for learning new words and expanding your vocabulary. With WordSlurp, you can import words from other applications, such as Google Translate, to create personalized word lists for study and practice.</p>
       <p>Start with importing your words from google translate!</p>
       <a href="https://github.com/domonkospapp/word-learning-frontend/wiki/How-to-import-words-from-Google-Translate">
         How can I import?
