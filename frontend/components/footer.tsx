a76e74891c06ed4bafb04e150d57cb40a04eae55
Domonkos Papp <domonkos.papp@regnology.net>
2023-01-18 00:49:11 +0100

    Style footer

--- frontend/components/footer.tsx
+++ frontend/components/footer.tsx
@@ -1,11 +1,14 @@
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 
 const Footer = () => (
-  <footer className={styles.footer}>
-    <span className={styles.logo}>
-      <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-    </span>
+  <footer className="mt-8 text-center">
+    <Image
+      src="/vercel.svg"
+      alt="Vercel Logo"
+      width={72}
+      height={16}
+      className="mx-auto"
+    />
   </footer>
 )
 export default Footer
