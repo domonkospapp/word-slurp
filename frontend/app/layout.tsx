83c808685896eeb9a8abe5aaded579b46f53bed0
Domonkos Papp <domonkos.papp@regnology.net>
2023-01-19 20:58:25 +0100

    Fix layour typos

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -12,7 +12,7 @@ const RootLayout = ({ children }: { children: ReactNode }) => {
       </head>
       <body className="bg-stone-200 font-space text-stone-900">
         <Providers>
-          <div className="max-w-normal mx-auto sm:mt-6 sm:mb-6">
+          <div className="mx-auto max-w-3xl sm:mt-6 sm:mb-6">
             <Navbar />
             <div className="border-stone-900 p-6 sm:border-b-4 sm:border-r-4 sm:border-l-4">
               {children}
