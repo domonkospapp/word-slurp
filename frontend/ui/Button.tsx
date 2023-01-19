c06244453e41e00ff1efde13d6f5884770145c25
Domonkos Papp <domonkos.papp@regnology.net>
2023-01-19 18:25:54 +0100

    Create button ui element

---
+++ frontend/ui/Button.tsx
@@ -0,0 +1,21 @@
+'use client'
+import React, { ReactNode } from 'react'
+
+const Button = ({
+  children,
+  onClick,
+  color,
+}: {
+  children: ReactNode
+  onClick: () => void
+  color: string
+}) => (
+  <button
+    className={`m-2 border-4 border-stone-900 ${color} p-2 font-bold shadow-3xl shadow-stone-900`}
+    onClick={onClick}
+  >
+    {children}
+  </button>
+)
+
+export default Button
