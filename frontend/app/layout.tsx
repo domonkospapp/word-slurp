55144079f33d33a7178fe7f71952392d08526823
Domonkos Papp <domonkos.papp@regnology.net>
2023-01-19 20:40:43 +0100

    Add layout styles

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -10,9 +10,9 @@ const RootLayout = ({ children }: { children: ReactNode }) => {
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </head>
-      <body className="bg-stone-200 font-space">
+      <body className="bg-stone-200 font-space text-stone-900">
         <Providers>
-          <div className="mx-auto max-w-3xl sm:mt-6 sm:mb-6">
+          <div className="max-w-normal mx-auto sm:mt-6 sm:mb-6">
             <Navbar />
             <div className="border-stone-900 p-6 sm:border-b-4 sm:border-r-4 sm:border-l-4">
               {children}
