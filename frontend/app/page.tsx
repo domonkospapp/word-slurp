b9b107819910bd3f4a70562de7c2d36644c5d210
Domonkos Papp <domonkos.papp@regnology.net>
2023-01-19 23:44:00 +0100

    Format home page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,10 +1,20 @@
 const Home = () => {
   return (
     <>
-      <h1>Word Slurp</h1>
-      <p>The WordSlurp app is a tool for learning new words and expanding your vocabulary. With WordSlurp, you can import words from other applications, such as Google Translate, to create personalized word lists for study and practice.</p>
-      <p>Start with importing your words from google translate!</p>
-      <a href="https://github.com/domonkospapp/word-learning-frontend/wiki/How-to-import-words-from-Google-Translate">
+      <h1 className="text-xl font-bold">Word Slurp</h1>
+      <p>
+        The WordSlurp app is a tool for learning new words and expanding your
+        vocabulary. With WordSlurp, you can import words from other
+        applications, such as Google Translate, to create personalized word
+        lists for study and practice.
+      </p>
+      <p className="font-bold">
+        Start with importing your words from google translate!
+      </p>
+      <a
+        className="underline"
+        href="https://github.com/domonkospapp/word-learning-frontend/wiki/How-to-import-words-from-Google-Translate"
+      >
         How can I import?
       </a>
     </>
