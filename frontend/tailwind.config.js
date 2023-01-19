ca091d8a74f1099bea5d9b5ebcbe46e9669b68bc
Domonkos Papp <domonkos.papp@regnology.net>
2023-01-19 20:37:05 +0100

    Create custom shadow

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -6,7 +6,12 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        normal: '6px 6px',
+        'b-normal': '0px 4px',
+      },
+    },
     fontFamily: {
       space: ['SpaceMono'],
     },
