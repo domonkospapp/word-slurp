5ac5e02cb96c0fde45294463b4320c401914ade2
Domonkos Papp <domonkos.papp@regnology.net>
2023-01-19 21:58:56 +0100

    Fix user login state

--- frontend/app/components/navbar/NavButton.tsx
+++ frontend/app/components/navbar/NavButton.tsx
@@ -8,7 +8,7 @@ const NavButton = () => {
       className="sm-text-2xl hover:text-stone-00 text-normal col-span-1 flex items-center justify-center border-b-4 border-l-4 border-stone-900 bg-stone-900 p-3.5 text-center font-bold text-stone-200 transition-all duration-150 sm:border-4 sm:bg-stone-200 sm:text-2xl sm:text-stone-900 hover:sm:bg-stone-200 hover:sm:text-stone-900"
       onClick={() => (loggedIn ? signOut() : signIn('google'))}
     >
-      {loggedIn ? 'LOGIN' : 'LOGOUT'}
+      {loggedIn ? 'LOGOUT' : 'LOGIN'}
     </button>
   )
 }
