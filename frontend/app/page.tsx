c4ebda6122d86a0cb5fca2be431ea692f4789752
Domonkos Papp <domonkos.papp@regnology.net>
2023-01-24 22:36:36 +0100

    Update home page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,23 +1,26 @@
+import ImportButton from './components/ImportButton'
+
 const Home = () => {
   return (
-    <>
-      <h1 className="text-xl font-bold">Word Slurp</h1>
-      <p>
+    <div>
+      <h1 className="m-2 text-xl font-bold">Word Slurp</h1>
+      <p className="m-2">
         The WordSlurp app is a tool for learning new words and expanding your
         vocabulary. With WordSlurp, you can import words from other
         applications, such as Google Translate, to create personalized word
         lists for study and practice.
       </p>
-      <p className="font-bold">
-        Start with importing your words from google translate!
+      <p className="m-2 font-bold">
+        Start importing your words from google translate!
       </p>
+      <ImportButton />
       <a
-        className="underline"
+        className="m-2 ml-10 underline"
         href="https://github.com/domonkospapp/word-learning-frontend/wiki/How-to-import-words-from-Google-Translate"
       >
         How can I import?
       </a>
-    </>
+    </div>
   )
 }
 export default Home
