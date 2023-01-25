eb32688bb15d0833a024a520af1ec0af11ca1851
Domonkos Papp <domonkos.papp@regnology.net>
2023-01-25 23:38:04 +0100

    Add disable possibility to select field

--- frontend/ui/inputs/Select.tsx
+++ frontend/ui/inputs/Select.tsx
@@ -6,15 +6,18 @@ const Select = ({
   children,
   onChange,
   value,
+  disabled,
 }: {
   children: ReactNode
   onChange: ChangeEventHandler<HTMLSelectElement>
   value: number | undefined
+  disabled?: boolean
 }) => (
   <select
     value={value}
     onChange={onChange}
     className="m-2 border-4 border-stone-900 bg-violet-300 p-2 font-bold shadow-normal shadow-stone-900"
+    disabled={disabled}
   >
     {children}
   </select>
