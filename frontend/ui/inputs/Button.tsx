4f0ef63ecb082fc67b24a2e275bf0e340806d570
Domonkos Papp <domonkos.papp@regnology.net>
2023-01-25 23:59:13 +0100

    Add disable possibility to buttons

--- frontend/ui/inputs/Button.tsx
+++ frontend/ui/inputs/Button.tsx
@@ -5,14 +5,17 @@ const Button = ({
   children,
   onClick,
   color,
+  disabled,
 }: {
   children: ReactNode
   onClick: () => void
   color: string
+  disabled?: boolean
 }) => (
   <button
     className={`m-2 border-4 border-stone-900 ${color} p-2 font-bold shadow-normal shadow-stone-900`}
     onClick={onClick}
+    disabled={disabled}
   >
     {children}
   </button>
