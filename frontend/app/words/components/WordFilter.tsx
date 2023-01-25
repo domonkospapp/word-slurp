c5860903a315ad434595bf9f339fa7cdf34257b5
Domonkos Papp <domonkos.papp@regnology.net>
2023-01-25 19:43:44 +0100

    Implement word set search

--- frontend/app/words/components/WordFilter.tsx
+++ frontend/app/words/components/WordFilter.tsx
@@ -1,16 +1,30 @@
 'use client'
 
+import { useRouter } from 'next/navigation'
+import { ChangeEvent, useState } from 'react'
+import Button from '../../../ui/inputs/Button'
 import Input from '../../../ui/inputs/Input'
 
 const WordFilter = () => {
+  const router = useRouter()
+  const [searchText, setSearchText] = useState<string>('')
+
+  const searchTextChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearchText(e.target.value)
+  }
+
+  const search = () => {
+    if (searchText == '') router.replace(`/words`)
+    else router.replace(`/words?search=${searchText}`)
+  }
+
   return (
-    <Input
-      onChange={(e) => {
-        console.log(e)
-      }}
-      value={'asd'}
-      fullWidth
-    />
+    <div className="flex">
+      <Input onChange={searchTextChange} value={searchText} fullWidth />
+      <Button onClick={search} color="bg-green-300">
+        Search
+      </Button>
+    </div>
   )
 }
 export default WordFilter
