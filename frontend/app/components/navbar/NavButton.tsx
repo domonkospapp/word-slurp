03691dcd86b0aceaaca06a491afe787357a1a6ab
Domonkos Papp <domonkos.papp@regnology.net>
2023-02-01 00:05:37 +0100

    Get nav button state on the server

--- frontend/app/components/navbar/NavButton.tsx
+++ frontend/app/components/navbar/NavButton.tsx
@@ -1,8 +1,6 @@
-import { signIn, signOut, useSession } from 'next-auth/react'
+import { signIn, signOut } from 'next-auth/react'
 
-const NavButton = () => {
-  const { data: session } = useSession()
-  const loggedIn = session?.user
+const NavButton = ({ loggedIn }: { loggedIn: boolean }) => {
   return (
     <button
       className="sm-text-2xl hover:text-stone-00 text-normal col-span-1 flex items-center justify-center border-b-4 border-l-4 border-stone-900 bg-stone-900 p-3.5 text-center font-bold text-stone-200 transition-all duration-150 sm:border-4 sm:bg-stone-200 sm:text-2xl sm:text-stone-900 hover:sm:bg-stone-200 hover:sm:text-stone-900"
