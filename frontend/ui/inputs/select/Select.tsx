47e4f18288a4a8f0349b2cfc1e23c52e91a07eb6
Domonkos Papp <domonkos.papp@regnology.net>
2023-03-05 19:41:18 +0100

    Change color and margin for testing

--- frontend/ui/inputs/select/Select.tsx
+++ frontend/ui/inputs/select/Select.tsx
@@ -12,18 +12,18 @@ const Select = ({
 }) => {
   return (
     <Listbox value={selected} onChange={update}>
-      <Listbox.Button className="shadow-stone-900` m-2 w-full border-4 border-stone-900 bg-violet-300 p-2 font-bold shadow-normal">
+      <Listbox.Button className="m-2 w-full border-4 border-stone-900 bg-violet-300 p-2 font-bold shadow-normal shadow-stone-900">
         <div className="flex justify-between">
           <span>{selected.text}</span>
           <span>&#9662;</span>
         </div>
       </Listbox.Button>
-      <Listbox.Options className="shadow-stone-900` absolute m-2 mt-0 border-4 border-stone-900 bg-violet-300 font-bold shadow-normal">
-        {options.map((v, k) => (
+      <Listbox.Options className="absolute ml-2 border-4 border-stone-900 bg-violet-300 font-bold shadow-normal shadow-stone-900">
+        {options.map((v) => (
           <Listbox.Option
-            key={k}
+            key={v.value}
             value={v}
-            className="p-2 ui-active:text-red-400"
+            className="p-2 ui-active:bg-stone-900 ui-not-active:bg-green-300"
           >
             {v.text}
           </Listbox.Option>
