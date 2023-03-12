dbb52d6847543f15cd158ca47dee30a1b2b7ef18
Domonkos Papp <domonkos.papp@regnology.net>
2023-03-12 17:10:26 +0100

    Rename

--- frontend/app/providers.tsx
+++ frontend/app/providers.tsx
@@ -2,7 +2,7 @@
 
 import { SessionProvider } from 'next-auth/react'
 import { ReactNode } from 'react'
-import { AnalyticsWrapper } from './components/common/AnalyticsR'
+import { AnalyticsWrapper } from './components/common/Analytics'
 
 const Providers = ({ children }: { children: ReactNode }) => {
   return (
